10c06916fe1cff8eaa902003127aa794c309b594
Michael Pham <michael.pham@accenture.com>
2012-05-24 09:16:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd3103b2b6b416b64ce1c8266bdcb30366c083663701fb3e140d2115a81a6a3e29f9c3422597a2a6d28d3f57c85699baea923b1527b876a1dd68c9ce2cc9d5174'
